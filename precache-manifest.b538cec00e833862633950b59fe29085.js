30b7f31ed78081db95d3d23f1c2ed65ae1670a5f
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-25 02:03:27 +0530

    Updates

---
+++ precache-manifest.b538cec00e833862633950b59fe29085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5780dd05b8f5a43904d0fcf2d368063",
+    "url": "/praveenorugantitech-todo-reactjs/index.html"
+  },
+  {
+    "revision": "520e486a7a89482684b2",
+    "url": "/praveenorugantitech-todo-reactjs/static/css/main.f3aa8ed8.chunk.css"
+  },
+  {
+    "revision": "f7c43c6aac8fd6fb8499",
+    "url": "/praveenorugantitech-todo-reactjs/static/js/2.e9293f7f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/praveenorugantitech-todo-reactjs/static/js/2.e9293f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520e486a7a89482684b2",
+    "url": "/praveenorugantitech-todo-reactjs/static/js/main.82d80c23.chunk.js"
+  },
+  {
+    "revision": "7d3c3c54eedf02bf5c13",
+    "url": "/praveenorugantitech-todo-reactjs/static/js/runtime-main.fd8519a4.js"
+  }
+]);
